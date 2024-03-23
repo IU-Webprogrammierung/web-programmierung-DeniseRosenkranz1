48f324760207cec28718356d24448e92d53e0840
DeniseRosenkranz1 <denise.rosenkranz@iu-study.org>
2024-03-23 18:47:07 +0100

    Accordion Styling + Zähler Produktseite

---
+++ js/counter.js
@@ -0,0 +1,11 @@
+var produkte = 0;
+
+function add() {
+  clicks += 1;
+  document.getElementById("produkte").innerHTML = clicks;
+};
+
+function subtract() {
+    clicks -= 1;
+    document.getElementById("produkte").innerHTML = clicks;
+  };
